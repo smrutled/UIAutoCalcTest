60bd760d9b95b4dadd846b08794a80e786c97f78
Sean Rutledge <smrutled@gmail.com>
2024-07-05 15:43:14 -0700

    Upgrade projects to .NET 8.0

--- CalcTesterTests/Properties/AssemblyInfo.cs
+++ CalcTesterTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CalcTesterTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalcTesterTests")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("2cfb0156-3459-45d7-8ae4-73ef46b47378")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
